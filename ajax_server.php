5b216d8162e41a528f0e0efb3257bb60000f4c8f
Ivan <ivan.scillone@gmail.com>
2026-06-20 13:45:45 +0200

    Ora la copia funziona

--- ajax_server.php
+++ ajax_server.php
@@ -22,8 +22,9 @@ $dbg->log($_GET);
 $r=new Result();
 switch ($_GET['action'] ?? false) {
     case 'copy':
-        if (file_exists($_GET['to'])) {
-            $r->data='<div class="alert alert-warning">Il file esiste già!</div>';
+        $overwrite = filter_var($_GET['overwrite'] ?? false, FILTER_VALIDATE_BOOLEAN);
+        if (file_exists($_GET['to']) && !$overwrite) {
+            $r->data='<div class="alert alert-warning alert-dismissible fade show">Il file esiste già!<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
         } else {
             $r->ok=copy($_GET['from'],$_GET['to']);
         }
